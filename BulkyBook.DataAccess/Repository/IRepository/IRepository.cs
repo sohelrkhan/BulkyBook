d1ba987d9ccad9c6078367b13c754ee169521e89
imrezratin <imrezratin@gmail.com>
2022-06-08 17:48:15 +0600

    IRepository

---
+++ BulkyBook.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace BulkyBook.DataAccess.Repository.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        void Add(T entity);
+    }
+}
